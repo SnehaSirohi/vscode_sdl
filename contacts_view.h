cea84670e4b5e80a6d37348d2072b676e1420f44
SnehaSirohi <105498235+SnehaSirohi@users.noreply.github.com>
2024-04-07 21:12:13 +0530

    Update

--- contacts_view.h
+++ contacts_view.h
@@ -8,7 +8,7 @@
 void contacts(lv_obj_t* parent);
 void add_contact_ui(lv_obj_t *parent);
 void createContactLabels(lv_obj_t *parent);
-int create_contact_profile(lv_obj_t *parent, char *name[]);
+void create_contact_profile(lv_obj_t *parent, char *name[]);
 lv_obj_t* create_phone_keypad(lv_obj_t* parent); 
-
+void popup_Box(lv_obj_t *parent, const char *name);
 #endif // CONTACTS_VIEW_H
